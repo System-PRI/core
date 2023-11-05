2fb386a679c404ccb71e845925d5d65c0e3411a7
Katarzyna Jaroszewska <katarzyna_jaroszewska@wp.eu>
2023-11-06 00:18:49 +0100

    feat: SYSPRI-176 - refactor - move save and update logic to separate services

---
+++ data-feed/src/main/java/pl/edu/amu/wmi/service/criteria/impl/CriteriaSaveServiceImpl.java
@@ -0,0 +1,96 @@
+package pl.edu.amu.wmi.service.criteria.impl;
+
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import pl.edu.amu.wmi.dao.CriteriaDAO;
+import pl.edu.amu.wmi.dao.CriteriaSectionDAO;
+import pl.edu.amu.wmi.entity.CriteriaGroup;
+import pl.edu.amu.wmi.entity.CriteriaSection;
+import pl.edu.amu.wmi.entity.Criterion;
+import pl.edu.amu.wmi.entity.EvaluationCardTemplate;
+import pl.edu.amu.wmi.enumerations.Semester;
+import pl.edu.amu.wmi.mapper.CriteriaGroupMapper;
+import pl.edu.amu.wmi.mapper.CriteriaSectionMapper;
+import pl.edu.amu.wmi.mapper.CriterionMapper;
+import pl.edu.amu.wmi.model.CriteriaGroupDTO;
+import pl.edu.amu.wmi.model.CriteriaSectionDTO;
+import pl.edu.amu.wmi.model.CriterionDTO;
+import pl.edu.amu.wmi.service.criteria.CriteriaSaveService;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+@Service
+@Transactional
+@Slf4j
+public class CriteriaSaveServiceImpl implements CriteriaSaveService {
+
+    private final CriteriaSectionMapper criteriaSectionMapper;
+    private final CriteriaGroupMapper criteriaGroupMapper;
+    private final CriterionMapper criterionMapper;
+    private final CriteriaSectionDAO criteriaSectionDAO;
+    private final CriteriaDAO criteriaDAO;
+
+    public CriteriaSaveServiceImpl(CriteriaSectionMapper criteriaSectionMapper,
+                                   CriteriaGroupMapper criteriaGroupMapper,
+                                   CriterionMapper criterionMapper,
+                                   CriteriaSectionDAO criteriaSectionDAO,
+                                   CriteriaDAO criteriaDAO) {
+        this.criteriaSectionMapper = criteriaSectionMapper;
+        this.criteriaGroupMapper = criteriaGroupMapper;
+        this.criterionMapper = criterionMapper;
+        this.criteriaSectionDAO = criteriaSectionDAO;
+        this.criteriaDAO = criteriaDAO;
+    }
+
+    @Override
+    public void saveCriteriaSection(CriteriaSectionDTO criteriaSectionDTO, EvaluationCardTemplate savedEvaluationCardTemplate, boolean isNewEvaluation) {
+        CriteriaSection firstSemesterCriteriaSection = criteriaSectionMapper.mapToEntityForFirstSemester(criteriaSectionDTO, isNewEvaluation);
+        CriteriaSection secondSemesterCriteriaSection = criteriaSectionMapper.mapToEntityForSecondSemester(criteriaSectionDTO, isNewEvaluation);
+
+        for (CriteriaGroupDTO criteriaGroupDTO : criteriaSectionDTO.criteriaGroups()) {
+            Set<Criterion> savedCriteria = saveCriteria(criteriaGroupDTO.criteria(), isNewEvaluation);
+
+            CriteriaGroup criteriaGroupForFirstSemester = createCriteriaGroup(criteriaGroupDTO, savedCriteria, Semester.SEMESTER_I, isNewEvaluation);
+            if (Objects.nonNull(criteriaGroupForFirstSemester)) {
+                firstSemesterCriteriaSection.getCriteriaGroups().add(criteriaGroupForFirstSemester);
+            }
+
+            CriteriaGroup criteriaGroupForSecondSemester = createCriteriaGroup(criteriaGroupDTO, savedCriteria, Semester.SEMESTER_II, isNewEvaluation);
+            if (Objects.nonNull(criteriaGroupForSecondSemester)) {
+                secondSemesterCriteriaSection.getCriteriaGroups().add(criteriaGroupForSecondSemester);
+            }
+        }
+
+        savedEvaluationCardTemplate.addCriteriaSectionForFirstSemester(firstSemesterCriteriaSection);
+        savedEvaluationCardTemplate.addCriteriaSectionForSecondSemester(secondSemesterCriteriaSection);
+
+        criteriaSectionDAO.save(firstSemesterCriteriaSection);
+        criteriaSectionDAO.save(secondSemesterCriteriaSection);
+    }
+
+    private CriteriaGroup createCriteriaGroup(CriteriaGroupDTO criteriaGroupDTO, Set<Criterion> savedCriteria, Semester semester, boolean isNewEvaluation) {
+        CriteriaGroup criteriaGroup = switch (semester) {
+            case SEMESTER_I -> criteriaGroupMapper.mapToEntityForFirstSemester(criteriaGroupDTO, isNewEvaluation);
+            case SEMESTER_II -> criteriaGroupMapper.mapToEntityForSecondSemester(criteriaGroupDTO, isNewEvaluation);
+        };
+        criteriaGroup.setCriteria(savedCriteria);
+        return isGradeWeightRelevant(criteriaGroup) ? criteriaGroup : null;
+    }
+
+    private boolean isGradeWeightRelevant(CriteriaGroup criteriaGroup) {
+        return Objects.nonNull(criteriaGroup.getGradeWeight()) && isWeightNotZero(criteriaGroup);
+    }
+
+    private boolean isWeightNotZero(CriteriaGroup criteriaGroup) {
+        return !(Math.abs(criteriaGroup.getGradeWeight()) < 1e-6);
+    }
+
+    private Set<Criterion> saveCriteria(List<CriterionDTO> criterionDTOS, boolean isNewEvaluation) {
+        List<Criterion> criteria = criterionMapper.mapToEntitiesList(criterionDTOS, isNewEvaluation);
+        return new HashSet<>(criteriaDAO.saveAll(criteria));
+    }
+}
---
+++ data-feed/src/main/java/pl/edu/amu/wmi/service/criteria/impl/CriteriaUpdateServiceImpl.java
@@ -0,0 +1,12 @@
+package pl.edu.amu.wmi.service.criteria.impl;
+
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import pl.edu.amu.wmi.service.criteria.CriteriaUpdateService;
+
+@Service
+@Transactional
+@Slf4j
+public class CriteriaUpdateServiceImpl implements CriteriaUpdateService {
+}
