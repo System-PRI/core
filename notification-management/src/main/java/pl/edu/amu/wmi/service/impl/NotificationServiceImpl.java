5050a573d237c8bb4b18fcb0c566c3284456b36a
Katarzyna Jaroszewska <katarzyna_jaroszewska@wp.eu>
2023-11-18 14:15:09 +0100

    feat: SYSPRI-186 - remove file email.properties

--- notification-management/src/main/java/pl/edu/amu/wmi/service/impl/NotificationServiceImpl.java
+++ notification-management/src/main/java/pl/edu/amu/wmi/service/impl/NotificationServiceImpl.java
@@ -6,7 +6,6 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -23,7 +22,6 @@ import java.util.Map;
 
 @Service
 @Slf4j
-@PropertySource(value = "classpath:email.properties")
 public class NotificationServiceImpl implements NotificationService {
 
     private static final String USER = "user";
