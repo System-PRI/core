587f8c0c736eed37d62be71f1b2008ecabf76cc7
Goora993 <gorkiewicz.dawid@gmail.com>
2023-12-08 21:22:23 +0100

    feat: SYSPRI-288 - Add GET supervisor availability survey api

--- defense-schedule/src/main/java/pl/edu/amu/wmi/service/supervisoravailability/impl/SupervisorAvailabilityServiceImpl.java
+++ defense-schedule/src/main/java/pl/edu/amu/wmi/service/supervisoravailability/impl/SupervisorAvailabilityServiceImpl.java
@@ -6,18 +6,29 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pl.edu.amu.wmi.dao.SupervisorDefenseAssignmentDAO;
 import pl.edu.amu.wmi.entity.SupervisorDefenseAssignment;
+import pl.edu.amu.wmi.mapper.supervisoravailability.SupervisorAvailabilityMapper;
 import pl.edu.amu.wmi.model.supervisordefense.SupervisorDefenseAssignmentDTO;
 import pl.edu.amu.wmi.service.supervisoravailability.SupervisorAvailabilityService;
+
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class SupervisorAvailabilityServiceImpl implements SupervisorAvailabilityService {
 
     private final SupervisorDefenseAssignmentDAO supervisorDefenseAssignmentDAO;
+    private final SupervisorAvailabilityMapper supervisorAvailabilityMapper;
 
     @Autowired
-    public SupervisorAvailabilityServiceImpl(SupervisorDefenseAssignmentDAO supervisorDefenseAssignmentDAO) {
+    public SupervisorAvailabilityServiceImpl(SupervisorDefenseAssignmentDAO supervisorDefenseAssignmentDAO,
+                                             SupervisorAvailabilityMapper supervisorAvailabilityMapper) {
         this.supervisorDefenseAssignmentDAO = supervisorDefenseAssignmentDAO;
+        this.supervisorAvailabilityMapper = supervisorAvailabilityMapper;
     }
 
     @Override
@@ -30,6 +41,53 @@ public class SupervisorAvailabilityServiceImpl implements SupervisorAvailability
         supervisorDefenseAssignmentEntity.setAvailable(isTimeSlotSelected);
         supervisorDefenseAssignmentDAO.save(supervisorDefenseAssignmentEntity);
         log.info("Supervisor availability was updated for supervisor with id: {}", supervisorId);
+    }
+
+    /**
+     * Returns Supervisor availability survey for the supervisor of a given study year.
+     */
+    @Override
+    public Map<String, List<SupervisorDefenseAssignmentDTO>> getSupervisorAvailabilitySurvey(String studyYear, Long supervisorId) {
+        List<SupervisorDefenseAssignment> supervisorDefenseAssignments = supervisorDefenseAssignmentDAO.findAllBySupervisor_Id(supervisorId);
+
+        Map<LocalDate, List<SupervisorDefenseAssignment>> supervisorDefenseAssignmentsByDate = mapSupervisorDefenseAssignmentsByDate(supervisorDefenseAssignments);
+
+        return createSupervisorAvailabilitySurvey(supervisorDefenseAssignmentsByDate);
+    }
+
+    /**
+     * Creates Supervisor defense assignments map aggregated by assignment date.
+     */
+    private Map<LocalDate, List<SupervisorDefenseAssignment>> mapSupervisorDefenseAssignmentsByDate(List<SupervisorDefenseAssignment> supervisorDefenseAssignments) {
+        return supervisorDefenseAssignments.stream()
+                .collect(Collectors.groupingBy(sda -> sda.getDefenseTimeSlot().getDate()));
+    }
+
+    /**
+     * Creates Supervisor availability survey based on Supervisor defense assignments aggregated by assignment date.
+     */
+    private Map<String, List<SupervisorDefenseAssignmentDTO>> createSupervisorAvailabilitySurvey(Map<LocalDate, List<SupervisorDefenseAssignment>> defenseAssignmentsByDate) {
+        Map<String, List<SupervisorDefenseAssignmentDTO>> supervisorAvailabilitySurvey = new TreeMap<>();
+        defenseAssignmentsByDate.keySet()
+                .forEach(key -> supervisorAvailabilitySurvey.put(key.toString(), convertDefenseAssignmentsToDtoWithSorting(defenseAssignmentsByDate.get(key))));
+        return supervisorAvailabilitySurvey;
+    }
+
+    /**
+     * Converts list of SupervisorDefenseAssignment to list of SupervisorDefenseAssignmentDTO sorted by assignment time.
+     */
+    private List<SupervisorDefenseAssignmentDTO> convertDefenseAssignmentsToDtoWithSorting(List<SupervisorDefenseAssignment> element) {
+        return supervisorAvailabilityMapper.mapToDtoList(element)
+                .stream()
+                .sorted(defenseAvailabilityByTimeComparator())
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Supervisor defense assignment time comparator.
+     */
+    private Comparator<? super SupervisorDefenseAssignmentDTO> defenseAvailabilityByTimeComparator() {
+        return Comparator.comparing(SupervisorDefenseAssignmentDTO::getTime);
     }
 
 }
